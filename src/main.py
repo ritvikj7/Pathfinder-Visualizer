e5cb311697c2fecd4d2342ce3e03b6fc3c2fe11c
ritvik <ritvikj@students.cs.ubc.ca>
2023-07-04 16:34:01 -0700

    organizing code

--- src/main.py
+++ src/main.py
@@ -3,35 +3,35 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-from Screen import Screen
-from map.Map import Map
-from PyQt5.QtWidgets import QApplication
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-# x = Node(False, 2, 3, "Regular")
-# print(x.get_y())
-app = QApplication([])
-
-map_obj = Map()
-
-screen = Screen(map_obj)
-screen.show()
-app.exec()
-
-
+# from Screen import Screen
 # from map.Map import Map
-# from algorithms.Dijkstras import Dijkstras
-# from algorithms.Astar import Astar
+# from PyQt5.QtWidgets import QApplication
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#
+# # x = Node(False, 2, 3, "Regular")
+# # print(x.get_y())
+# app = QApplication([])
 #
 # map_obj = Map()
 #
-# astar_obj = Astar(3, 3, map_obj.get_array())
-# distance1 = astar_obj.astar(4, 4)
-# print(distance1)
-#
-# d_obj = Dijkstras(3, 3, map_obj.get_array())
-# distance2 = d_obj.dijkstras()
-# print(distance2)
+# screen = Screen(map_obj)
+# screen.show()
+# app.exec()
+
+
+from map.Map import Map
+from algorithms.Dijkstras import Dijkstras
+from algorithms.Astar import Astar
+
+map_obj = Map()
+
+astar_obj = Astar(3, 3, map_obj.get_array())
+distance1 = astar_obj.astar(4, 4)
+print(distance1)
+
+d_obj = Dijkstras(3, 3, map_obj.get_array())
+distance2 = d_obj.dijkstras()
+print(distance2)
 
 
--- src/map/Map.py
+++ src/map/Map.py
@@ -1,6 +1,6 @@
-
 from .Node import Node
 import random
+
 
 class Map:
 
@@ -8,26 +8,33 @@ class Map:
         self.node_array = self.setup()
 
     def setup(self):
+        # random.seed(967)
+        # headx = random.randint(0, 9)
+        # heady = random.randint(0, 9)
+        # tailx = random.randint(0, 9)
+        # taily = random.randint(0, 9)
+
         # width, arbitrary value
-        random.seed(967)
-        headx = random.randint(0, 9)
-        heady = random.randint(0, 9)
-        tailx = random.randint(0, 9)
-        taily = random.randint(0, 9)
-
-        cols = 10
+        cols = 5
         # height, arbitrary value
-        rows = 10
-        #filling the array
-        array = [[Node(True, x, y, "head",0) if x == headx and y==heady else Node(True, x, y, "tail",0) if x == tailx and y==taily else Node(True, x, y, "block",0)
-                  for x in range(cols)] for y in range(rows)]
+        rows = 5
 
+        # array = [[Node(True, x, y, "head", 0) if x == headx and y == heady else Node(True, x, y, "tail",
+        #                                                                              0) if x == tailx and y == taily else Node(
+        #     True, x, y, "block", 0)
+        #           for x in range(cols)] for y in range(rows)]
+
+        # filling the array
+        array = [[Node(True, 0, 0, "regular", 0) for y in range(cols)] for x in range(rows)]
+        self.fill_array(array, cols, rows)
+
+        return array
+
+    def fill_array(self, array, cols, rows):
         for i in range(rows):
             for j in range(cols):
                 array[i][j].x = j
                 array[i][j].y = i
-
-
 
     def get_array(self):
         return self.node_array
