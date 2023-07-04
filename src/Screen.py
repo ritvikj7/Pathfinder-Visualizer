ef5eb2f86e9d4abe898fac58d252af3e82fe3b22
sus194 <spa194@sfu.ca>
2023-07-04 15:16:11 -0700

    wokring on moving head and tails

--- src/Screen.py
+++ src/Screen.py
@@ -2,14 +2,19 @@
 
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsView, QGraphicsEllipseItem, \
-QGraphicsRectItem, QToolBar, QAction
+    QGraphicsRectItem, QToolBar, QAction, QPushButton, QGraphicsTextItem
 from PyQt5.QtGui import QBrush, QColor, QPen, QFont
 from PyQt5.QtCore import Qt, QPointF
 
 class Screen(QMainWindow):
     def __init__(self, map_obj):
         super().__init__()
-
+        #booleans for determing if the head or the tail should be moved or not
+        self.headchange = False
+        self.tailchange = False
+        #position of the head and th tail
+        self.headpos =[]
+        self.tailpos =[]
         self.setWindowTitle("Matrix Visualization")
 
         self.scene = QGraphicsScene()
@@ -21,6 +26,7 @@ class Screen(QMainWindow):
 
         self.num_rows = 10
         self.num_cols = 10
+
 
         # Calculate the size of each box
         self.box_width = self.view.width() / self.num_cols
@@ -37,12 +43,14 @@ class Screen(QMainWindow):
     def create_toolbar(self):
         toolbar = QToolBar()
         self.addToolBar(toolbar)
-
-        action1 = QAction("head", self)
-        toolbar.addAction(action1)
-
-        action2 = QAction("tail", self)
-        toolbar.addAction(action2)
+        # Create the navbar icon button
+        self.head_icon_button = QPushButton("Head", self)
+        toolbar.addWidget(self.head_icon_button)
+        self.tail_icon_button = QPushButton("Tail", self)
+        toolbar.addWidget(self.tail_icon_button)
+        # Connect the button's clicked signal to the head or tail movement method
+        self.head_icon_button.clicked.connect(self.handle_head_change)
+        self.tail_icon_button.clicked.connect(self.handle_tail_change)
 
 
     #for drawing the map
@@ -56,17 +64,24 @@ class Screen(QMainWindow):
                 pen = QPen(Qt.NoPen)
                 #for specifying the head or the tail
                 text =""
-                if self.map_obj.get_array()[j][i].classification == "head":
+                if self.map_obj.get_array()[i][j].classification == "head":
+                    self.headpos.append(j)
+                    self.headpos.append(i)
                     brush = QBrush(Qt.green)
                     text = "Head"
 
-                if self.map_obj.get_array()[j][i].classification == "tail":
+
+                if self.map_obj.get_array()[i][j].classification == "tail":
+                    self.tailpos.append(j)
+                    self.tailpos.append(i)
                     brush = QBrush(Qt.green)
                     text = "Tail"
 
+
+
                 #drwing the map
                 self.scene.addRect(x, y, self.box_width, self.box_height, pen, brush)
-                #adding text to head and tails
+                #adding text
                 text_item = self.scene.addText(text, QFont("Arial", 10))
                 text_item.setPos(x + self.box_width / 2 - text_item.boundingRect().width() / 2,
                                  y + self.box_height / 2 - text_item.boundingRect().height() / 2)
@@ -83,12 +98,38 @@ class Screen(QMainWindow):
 
         clicked_item = self.scene.itemAt(scene_pos, self.view.transform())
 
-        if isinstance(clicked_item, QGraphicsRectItem) and self.map_obj.get_array()[x][y].classification == "block":
-            if self.map_obj.get_array()[x][y].free:
+        if isinstance(clicked_item, QGraphicsRectItem):
+            if self.headchange:
+                if self.map_obj.get_array()[x][y].classification == "block":
+
+                    # Move the head block
+                    print(self.headpos)
+                    head_item = self.scene.itemAt(self.headpos[0], self.headpos[1], self.view.transform())
+                    head_item.setBrush(QBrush(Qt.white))
+                    text_item = self.scene.itemAt(self.headpos[0] * self.box_width, self.headpos[1] * self.box_height, self.view.transform())
+                    if isinstance(text_item, QGraphicsTextItem):
+                        # Remove the text item from the scene
+                        self.scene.removeItem(text_item)
+
+                    # Update the position of the rectangle item
+                    clicked_item.setRect(x * self.box_width, y * self.box_height, self.box_width, self.box_height)
+                    clicked_item.setBrush(QBrush(Qt.green))
+            elif self.map_obj.get_array()[x][y].free and self.map_obj.get_array()[x][y].classification == "block":
                 self.map_obj.get_array()[x][y].block_node()
                 print(self.map_obj.get_array()[x][y].free)
                 clicked_item.setBrush(QBrush(Qt.black))
-            else:
+            elif not self.map_obj.get_array()[x][y].free and self.map_obj.get_array()[x][y].classification == "block":
                 self.map_obj.get_array()[x][y].free_node()
                 print(self.map_obj.get_array()[x][y].free)
                 clicked_item.setBrush(QBrush(Qt.white))
+
+    def handle_head_change(self):
+        self.headchange = not self.headchange  # Toggle the headchange variable
+        self.tailchange = False
+    def handle_tail_change(self):
+        self.tailchange = not self.tailchange  # Toggle the headchange variable
+        self.headchange = False
+
+
+
+
