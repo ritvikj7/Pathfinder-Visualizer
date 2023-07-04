d45ce159d8cd458bacbd104fddec2bcf103e546f
sus194 <spa194@sfu.ca>
2023-07-04 15:20:55 -0700

    commented and uncommented

--- src/main.py
+++ src/main.py
@@ -11,27 +11,26 @@ from PyQt5.QtWidgets import QApplication
 
 # x = Node(False, 2, 3, "Regular")
 # print(x.get_y())
-app = QApplication([])
+#app = QApplication([])
+#map_obj = Map()
+
+#screen = Screen(map_obj)
+#screen.show()
+#app.exec()
+
+
+from map.Map import Map
+from algorithms.Dijkstras import Dijkstras
+from algorithms.Astar import Astar
 
 map_obj = Map()
 
-screen = Screen(map_obj)
-screen.show()
-app.exec()
+astar_obj = Astar(3, 3, map_obj.get_array())
+distance1 = astar_obj.astar(4, 4)
+print(distance1)
 
-
-# from map.Map import Map
-# from algorithms.Dijkstras import Dijkstras
-# from algorithms.Astar import Astar
-#
-# map_obj = Map()
-#
-# astar_obj = Astar(3, 3, map_obj.get_array())
-# distance1 = astar_obj.astar(4, 4)
-# print(distance1)
-#
-# d_obj = Dijkstras(3, 3, map_obj.get_array())
-# distance2 = d_obj.dijkstras()
-# print(distance2)
+d_obj = Dijkstras(3, 3, map_obj.get_array())
+distance2 = d_obj.dijkstras()
+print(distance2)
 
 
